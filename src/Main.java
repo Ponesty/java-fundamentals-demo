ca1611450b000a0d01a1b3031b562676fca00143
Jerrell Prier <jerrellprier@gmail.com>
2022-01-01 17:01:53 -0600

    Created OOM9 battledroid and C3PO protocoldroid. Created an array of type droid and added the droids to the array.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Tool> r2Tools = new ArrayList<>();
         ArrayList<Tool> b1Tools = new ArrayList<>();
+        ArrayList<Tool> OOM9Tools = new ArrayList<>();
+        ArrayList<Tool> C3POTools = new ArrayList<>();
+
+
         Tool repairTool1 = new Tool(ToolType.STARSHIP_REPAIR);
         Tool repairTool2 = new Tool(ToolType.STARSHIP_REPAIR);
         Tool repairTool3 = new Tool(ToolType.STARSHIP_REPAIR);
@@ -14,10 +18,21 @@ public class Main {
         r2Tools.add(repairTool3);
 
         Tool weapon1 = new Tool(ToolType.RIFLES);
+        Tool weapon2 = new Tool(ToolType.RIFLES);
         b1Tools.add(weapon1);
+        OOM9Tools.add(weapon2);
 
         AstromechDroid R2 = new AstromechDroid("R2D2", ModelFactory.getModel(1), "Starship repair/support", r2Tools);
         BattleDroid B1 = new BattleDroid("B1", ModelFactory.getModel(3), "Combat", b1Tools);
+
+        BattleDroid OOM9 = new BattleDroid("OOM-9",ModelFactory.getModel(3),"Combat",OOM9Tools);
+
+        ProtocolDroid C3PO = new ProtocolDroid("C3PO",ModelFactory.getModel(2),"Translation",C3POTools);
+
+        ArrayList<Droid> droidArrayList = new ArrayList<>();
+        droidArrayList.add(R2);
+        droidArrayList.add(OOM9);
+        droidArrayList.add(C3PO);
 
         System.out.println(R2.getPurpose());
         System.out.println(R2.speak());
--- src/ProtocolDroid.java
+++ src/ProtocolDroid.java
@@ -4,4 +4,7 @@ public class ProtocolDroid extends Droid {
     public ProtocolDroid(String serial, String type, String purpose, ArrayList<Tool> tool) {
         super(serial, type, purpose, tool);
     }
+    public String speak() {
+        return "We'll Hello there!";
+    }
 }
